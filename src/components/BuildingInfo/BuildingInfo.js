629162911e80eb7d276fa509b667cd178ba8543d
firstname lastname <firstname_surname@firstnamesurname.com>
2024-06-24 17:09:23 -0400

    modify variable names for BuildingInfo to adapt to database structure changes

--- src/components/BuildingInfo/BuildingInfo.js
+++ src/components/BuildingInfo/BuildingInfo.js
@@ -2,8 +2,8 @@ export function BuildingInfo ({ selectedBuilding }) {
     return( selectedBuilding.map( (property) => 
       <div key={ property.building_id }> 
         <p>NOM: { property.name }</p>
-        <p>ADRESSE: { property.streetnumber} , { property.street }</p>
-        <p>{ property.postalcode } { property.town }</p>
+        <p>ADRESSE: { property.street_number} , { property.street}</p>
+        <p>{ property.zip_code } { property.city }</p>
         <p>SUPERFICIE: { property.surface } pi.ca</p>
       </div> )
     )
